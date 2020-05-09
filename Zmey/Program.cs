417eaea8987617acccbd33c92e98d7dbed278220
Dmitrii <ldformail@mail.ru>
2020-05-10 00:51:03 +0300

    initial commit

---
+++ Zmey/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Zmey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
